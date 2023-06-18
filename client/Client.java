91f10ced52f2c35bea0b82801e2f5efcd38312de
Sam Graham <samagraham2003@gmail.com>
2023-06-18 01:00:35 -0700

    Add client and server

---
+++ client/Client.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.net.*;
+
+public class Client {
+
+    private static final String SERVER_IP = "127.0.0.1";
+    private static final int SERVER_PORT = 8080;
+
+    public static void main(String[] args) {
+
+        try ( // try-with-resources handles auto-closing of resources on exit or catch
+            Socket socket = new Socket(SERVER_IP, SERVER_PORT);
+
+            InputStream inputStream = socket.getInputStream();
+            BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+
+            OutputStream outputStream = socket.getOutputStream();
+            PrintWriter out = new PrintWriter(outputStream, true);
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ) {
+            while (true) {
+                // Get user input
+                System.out.print("Enter message ('exit' to quit): ");
+                String message = reader.readLine();
+
+                if (message.equalsIgnoreCase("exit")) {
+                    break;
+                }
+
+                // Send message to server
+                out.println(message);
+
+                // Receive message from server
+                String response = in.readLine();
+                if (response == null) {
+                    System.out.println("Server closed. Exiting");
+                    break;
+                }
+                System.out.println("Server: " + response);
+            }
+        } catch (ConnectException e) {
+            System.err.println("Error: Connection refused. Make sure the server is running.");
+        } catch (SocketTimeoutException e) {
+            System.err.println("Error: Connection timed out. Check your network connection.");
+        } catch (IOException e) {
+            System.err.println("Error occurred while communicating with the server: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
---
+++ server/server.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <signal.h>
+
+#define PORT 8080
+#define REUSE_ADDR
+
+#define BUFFER_SIZE 1024
+
+int server_socket_fd = -1;
+int client_socket_fd = -1;
+
+void cleanup() {
+    puts("");
+    if (client_socket_fd != -1) {
+        puts("closing client socket");
+        close(client_socket_fd);
+        client_socket_fd = -1;
+    }
+    if (server_socket_fd != -1) {
+        puts("closing server socket");
+        close(server_socket_fd);
+        server_socket_fd = -1;
+    }
+}
+
+void sigint_handler(int signum) {
+    cleanup();
+    exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char** argv) {
+    /* Register SIGINT signal handler */
+    __sighandler_t signal_result = signal(SIGINT, sigint_handler);
+    if (signal_result == SIG_ERR) {
+        perror("Error: signal()");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Configure server address info */
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET; /* IPV4 */
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    /* Create server socket */
+    server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket_fd == -1) {
+        perror("Error: socket()\n");
+        exit(EXIT_FAILURE);
+    }
+
+#ifdef REUSE_ADDR
+    /* Bypass socket TIME_WAIT status when restarting server */
+    int server_socket_opt_result = setsockopt(server_socket_fd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int));
+    if (server_socket_opt_result == -1) {
+        perror("Error: setsockopt()");
+        cleanup();
+        exit(EXIT_FAILURE);
+    }
+#endif
+
+    /* Bind server socket to address info */
+    int bind_result = bind(server_socket_fd, (struct sockaddr*) &server_addr, sizeof(server_addr));
+    if (bind_result == -1) {
+        perror("Error: bind()\n");
+        cleanup();
+        exit(EXIT_FAILURE);
+    }
+
+    /* Listen for incoming connections */
+    int listen_result = listen(server_socket_fd, 1);
+    if (listen_result == -1) {
+        perror("Error: listen()\n");
+        cleanup();
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, sizeof(buffer));
+
+    while (1) {
+        printf("Listening for connections on port %i...\n", ntohs(server_addr.sin_port));
+
+        /* Await a connection */
+        client_socket_fd = accept(server_socket_fd, NULL, NULL);
+        if (client_socket_fd == -1) {
+            perror("Error: accept()");
+            cleanup();
+            exit(EXIT_FAILURE);
+        } else {
+            puts("Client accepted");
+        }
+
+        while (1) {
+            /* Recieve message from client */
+            memset(buffer, 0, sizeof(buffer));
+            ssize_t recv_size = recv(client_socket_fd, buffer, sizeof(buffer) - 1, 0);
+            if (recv_size == -1) {
+                perror("Error: recv()");
+                cleanup();
+                exit(EXIT_FAILURE);
+            } else if (recv_size == 0) {
+                puts("Client disconnected");
+                if (client_socket_fd != -1) {
+                    puts("closing client socket");
+                    close(client_socket_fd);
+                    client_socket_fd = -1;
+                }
+                break;
+            }
+            printf("Client: %s", buffer);
+
+            /* Get user input */
+            fputs("Enter message ('exit' to quit): ", stdout);
+            memset(buffer, 0, sizeof(buffer));
+            fgets(buffer, sizeof(buffer), stdin);
+
+            if (strcmp(buffer, "exit\n") == 0) {
+                cleanup();
+                exit(EXIT_SUCCESS);
+            }
+
+            /* Send message to client */
+            ssize_t send_size = send(client_socket_fd, buffer, strlen(buffer), 0);
+            if (send_size == -1) {
+                perror("Error: send()");
+                cleanup();
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    puts("Closing session");
+    cleanup();
+
+    return 0;
+}
